58eeb0140b61ea38cff40e4d0367094534dab398
Nikolai <nikolaimoiseev01@gmail.com>
2026-02-23 21:13:57 +0300

    First Commit

--- resources/views/livewire/pages/portal/index-page.blade.php
+++ resources/views/livewire/pages/portal/index-page.blade.php
@@ -1,20 +1,4 @@
 <main class="flex-1">
-    {{--    <section class="container content flex gap-8 p-8">--}}
-    {{--        <div class="flex flex-col gap-16">--}}
-    {{--            <p class="text-2xl">--}}
-    {{--                Для всех, кто хочет освоить мощный инструмент нового поколения - нейросети. Мы--}}
-    {{--                собрали для Вас лучшие видео и статьи по нейросетям разного уровня сложности - от--}}
-    {{--                освоения ЯндексGPT до принципов создания Al-помощников и чат-ботов. Вы платите 1 раз--}}
-    {{--                и получаете бессрочный доступ ко всем нашим материалам, которые постоянно--}}
-    {{--                обновляются.--}}
-    {{--            </p>--}}
-    {{--            <div class="flex gap-4">--}}
-    {{--                <x-ui.link class="text-xl px-12 py-4">Вступить в клуб</x-ui.link>--}}
-    {{--                <x-ui.link class="text-xl px-12 py-4" variant="dark">Консультация</x-ui.link>--}}
-    {{--            </div>--}}
-    {{--        </div>--}}
-    {{--        <img src="/fixed/welcome-img.png" class="w-1/3 rounded-xl aspect-square object-cover" alt="">--}}
-    {{--    </section>--}}
     <x-index-blocks.hero-block/>
 
     <x-index-blocks.why-platform/>
